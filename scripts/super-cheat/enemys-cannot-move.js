632c2aa2e71015e2935368e7b9828edea03f8a64
abomb4 <yangrlx@gmail.com>
2020-10-24 14:26:05 +0800

    4 shields

---
+++ scripts/super-cheat/enemys-cannot-move.js
@@ -0,0 +1,97 @@
+
+const lib = require('super-cheat/lib');
+
+const cannotMoveEffect = new StatusEffect("cannotMove");
+
+cannotMoveEffect.speedMultiplier = 0;
+cannotMoveEffect.effect = Fx.none;
+
+const shieldConsumer = (paramEntity) => cons(trait => {
+    if (trait.team != paramEntity.team
+        && Intersector.isInsideHexagon(paramEntity.x, paramEntity.y, paramEntity.realRadius() * 2, trait.x, trait.y)) {
+            trait.apply(cannotMoveEffect, 30);
+    }
+});
+const COLOR = new Color(0, 200, 250, 1);
+const blockType = extendContent(ForceProjector, "enemys-cannot-move", {});
+lib.setBuildingSimple(blockType, ForceProjector.ForceBuild, {
+    updateTile() {
+        var phaseValid = this.block.consumes.get(ConsumeType.item).valid(this);
+
+        this.phaseHeat = Mathf.lerpDelta(this.phaseHeat, Mathf.num(phaseValid), 0.1);
+
+        if (phaseValid && !this.broken && this.timer.get(blockType.timerUse, blockType.phaseUseTime) && this.efficiency() > 0) {
+            this.consume();
+        }
+
+        this.radscl = Mathf.lerpDelta(this.radscl, this.broken ? 0 : this.warmup, 0.05);
+
+        if (Mathf.chanceDelta(this.buildup / this.breakage * 0.1)) {
+            Fx.reactorsmoke.at(this.x + Mathf.range(tilesize / 2), this.y + Mathf.range(tilesize / 2));
+        }
+
+        this.warmup = Mathf.lerpDelta(this.warmup, this.efficiency(), 0.1);
+
+        if (this.buildup > 0) {
+            var scale = !this.broken ? cooldownNormal : this.cooldownBrokenBase;
+            var cons = this.block.consumes.get(ConsumeType.liquid);
+            if (cons.valid(this)) {
+                cons.update(this);
+                scale *= (cooldownLiquid * (1 + (this.liquids.current().heatCapacity - 0.4) * 0.9));
+            }
+
+            this.buildup -= delta() * scale;
+        }
+
+        if (this.broken && this.buildup <= 0) {
+            this.broken = false;
+        }
+
+        if (this.buildup >= this.breakage + this.phaseShieldBoost && !this.broken) {
+            this.broken = true;
+            this.buildup = this.breakage;
+            Fx.shieldBreak.at(this.x, this.y, this.realRadius(), this.team.color);
+        }
+
+        if (this.hit > 0) {
+            this.hit -= 1 / 5 * Time.delta;
+        }
+
+        var realRadius = this.realRadius();
+
+        if (realRadius > 0 && !this.broken) {
+            Groups.unit.intersect(this.x - realRadius, this.y - realRadius, realRadius * 2, realRadius * 2, shieldConsumer(this));
+        }
+    },
+    drawShield() {
+        var x = this.x;
+        var y = this.y;
+        var hit = this.hit;
+        if (!this.broken) {
+            var radius = this.realRadius();
+
+            Draw.z(Layer.shields);
+
+
+            if (Core.settings.getBool("animatedshields")) {
+                Draw.color(COLOR, Color.white, Mathf.clamp(hit));
+                Fill.poly(x, y, 6, radius);
+                Draw.color(this.team.color, Color.white, Mathf.clamp(hit));
+                Lines.poly(x, y, 6, radius);
+            } else {
+                Lines.stroke(1.5);
+                Draw.alpha(0.09 + Mathf.clamp(0.08 * hit));
+                Draw.color(COLOR, Color.white, Mathf.clamp(hit));
+                Fill.poly(x, y, 6, radius);
+                Draw.alpha(1);
+                Draw.color(this.team.color, Color.white, Mathf.clamp(hit));
+                Lines.poly(x, y, 6, radius);
+                Draw.reset();
+            }
+        }
+
+        Draw.reset();
+    },
+    // damage(damage) { },
+    // handleDamage(tile, amount) { return 0; },
+});
