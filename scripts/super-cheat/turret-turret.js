2817348347d3cc541fe77b92ddd53ca125843bcb
abomb4 <yangrlx@gmail.com>
2020-11-18 01:01:49 +0800

    wow 2.5.1

--- scripts/super-cheat/turret-turret.js
+++ scripts/super-cheat/turret-turret.js
@@ -11,22 +11,27 @@ function bulletTypesOfItemTurret(turret) {
     }
     return arr;
 }
+const hitEffects = {
+    1: Fx.hitBulletBig,
+    2: Fx.flakExplosion,
+    3: Fx.blastExplosion,
+    4: Fx.massiveExplosion
+};
 function defineTurretBullet(turret, bullets, options) {
+    const tr = new Vec2();
     if (!(bullets instanceof Array)) {
         bullets = [bullets];
     }
     const finalOptions = Object.assign({
         shotChance: 0.025,
-        speed: 2.5,
-        lifetime: 150,
-        damage: turret.health / 20,
+        speed: 2.8,
+        lifetime: 240,
+        damage: 20,
         shrinkY: 0,
-        drag : -0.01,
-        splashDamageRadius: 30,
-        splashDamage: 20,
+        drag: -0.01,
+        splashDamageRadius: 16 * turret.size,
+        splashDamage: turret.health / 15,
         ammoMultiplier: 4,
-        hitEffect: Fx.blastExplosion,
-        despawnEffect: Fx.blastExplosion,
         homingPower: 0.08,
         homingRange: 100,
         homingDelay: 20,
@@ -34,22 +39,101 @@ function defineTurretBullet(turret, bullets, options) {
         trailChance: 0,
         status: StatusEffects.blasted,
         statusDuration: 60,
-        spin: 8,
+        spin: 4,
+        hitEffect: hitEffects[turret.size],
+        despawnEffect: hitEffects[turret.size],
         backColor: Color.valueOf("00000000"),
         frontColor: Color.valueOf("beacbe"),
     }, options);
     const name = turret.name;
-    var t = new JavaAdapter(BasicBulletType, {
-        update(b) {
-            this.super$update(b);
-            if (Mathf.chanceDelta(finalOptions.shotChance)) {
-                turret.shootSound.at(b.x, b.y, Mathf.random(0.9, 1.1));
-                var angle = b.time * this.spin + b.vel.angle();
-                bullets[randomInt(0, bullets.length)].create(b, b.team, b.x, b.y, angle, 0.8, 0.8);
-                turret.shootEffect.at(b.x, b.y, angle);
+
+    var t = (() => {
+        return new JavaAdapter(BasicBulletType, {
+            init(b) {
+                if (b) {
+                    this.super$init(b);
+                    b.data = {
+                        shotCounter: 0,
+                        bulletLife: 0,
+                        bullet: null,
+                        spinDirection: [1, -1][Math.floor(Math.random() * 2)]
+                    };
+                }
+            },
+            draw(b) {
+                var shrinkY = this.shrinkY;
+                var shrinkX = this.shrinkX;
+                var height = this.height * ((1 - shrinkY) + shrinkY * b.fout());
+                var width = this.width * ((1 - shrinkX) + shrinkX * b.fout());
+                var offset = -90 + (this.spin != 0 ? b.time * this.spin * b.data.spinDirection : 0);
+                Draw.color(this.frontColor);
+                Draw.rect(this.frontRegion, b.x, b.y, width, height, b.rotation() + offset);
+                Draw.reset();
+            },
+            update(b) {
+                this.super$update(b);
+
+                var offset = (this.spin != 0 ? b.time * this.spin * b.data.spinDirection : 0);
+                var angle = b.rotation() + offset;
+                tr.trns(angle, turret.size * Vars.tilesize / 2);
+                var x = b.x + tr.x;
+                var y = b.y + tr.y;
+
+                if (b.data.bulletLife > 0) {
+                    if (b.data.bullet) {
+                        b.data.bulletLife = Math.max(0, b.data.bulletLife - 1);
+                        b.data.bullet.rotation(angle);
+                        b.data.bullet.set(x, y);
+                        b.data.bullet.time = 0;
+                        if (b.data.bulletLife <= 0) {
+                            b.data.bullet = null;
+                        }
+                    } else {
+                        b.data.bulletLife = 0;
+                    }
+                }
+
+                if (Mathf.chanceDelta(finalOptions.shotChance)) {
+                    turret.shootSound.at(b.x, b.y, Mathf.random(0.9, 1.1));
+                    var bt = bullets[randomInt(0, bullets.length)];
+
+                    if (turret.shootDuration > 0) {
+                        // for meltdown
+                        if (b.data.bulletLife <= 0) {
+                            b.data.bulletLife = turret.shootDuration;
+                            b.data.bullet = bt.create(b, b.team, x, y, angle, 1, 0.8);
+                            turret.shootEffect.at(x, y, angle);
+                        }
+                    } else if (turret.burstSpacing > 0.0001) {
+                        for (var i = 0; i < turret.shots; i++) {
+                            Time.run(turret.burstSpacing * i, run(() => {
+                                bt.create(b, b.team, x, y, angle + Mathf.range(turret.inaccuracy + bt.inaccuracy), 0.8, 0.8);
+                                turret.shootEffect.at(x, y, angle);
+                                if (bt.shootEffect) {
+                                    bt.shootEffect.at(x, y, angle);
+                                }
+                            }));
+                        }
+                    } else {
+                        turret.shootEffect.at(x, y, angle);
+                        if (bt.shootEffect) {
+                            bt.shootEffect.at(x, y, angle);
+                        }
+                        if (turret.alternate) {
+                            var i = (b.data.shotCounter % turret.shots) - (turret.shots - 1) / 2;
+                            bt.create(b, b.team, x, y, angle, 0.8, 0.8);
+                        } else {
+                            for (var i = 0; i < turret.shots; i++) {
+                                bt.create(b, b.team, x, y, angle + (i - (turret.shots / 2)) * turret.spread, 0.8 + Mathf.range(turret.velocityInaccuracy), 0.8);
+                            }
+                        }
+                        b.data.shotCounter++;
+                    }
+
+                }
             }
-        }
-    }, finalOptions.speed, finalOptions.damage, name);
+        }, finalOptions.speed, finalOptions.damage, name);
+    })();
 
     t.width = turret.size * 8;
     t.height = turret.size * 8;
@@ -85,21 +169,21 @@ const bulletTypes = (() => {
     function di(turret, options) {
         all.push(defineTurretBullet(turret, bulletTypesOfItemTurret(turret), options));
     }
-    di(Blocks.duo, { shotChance: 0.03 });
-    di(Blocks.scatter, { shotChance: 0.05 });
+    di(Blocks.duo, { shotChance: 0.035 });
+    di(Blocks.scatter, { shotChance: 0.035 });
     di(Blocks.scorch, { shotChance: 0.06 });
-    di(Blocks.hail, { shotChance: 0.03 });
+    di(Blocks.hail, { shotChance: 0.035 });
     di(Blocks.wave, { shotChance: 0.1 });
     d(Blocks.lancer, Blocks.lancer.shootType, { shotChance: 0.03 });
-    d(Blocks.arc, Blocks.arc.shootType, { shotChance: 0.03 });
-    di(Blocks.swarmer, { shotChance: 0.03 });
-    di(Blocks.salvo, { shotChance: 0.06 });
+    d(Blocks.arc, Blocks.arc.shootType, { shotChance: 0.04 });
+    di(Blocks.swarmer, { shotChance: 0.035 });
+    di(Blocks.salvo, { shotChance: 0.035 });
     di(Blocks.tsunami, { shotChance: 0.1 });
-    di(Blocks.fuse, { shotChance: 0.04 });
-    di(Blocks.ripple, { shotChance: 0.05 });
-    di(Blocks.cyclone, { shotChance: 0.1 });
-    di(Blocks.spectre, { shotChance: 0.1 });
-    d(Blocks.meltdown, Blocks.meltdown.shootType, { shotChance: 0.05 });
+    di(Blocks.fuse, { shotChance: 0.035 });
+    di(Blocks.ripple, { shotChance: 0.02 });
+    di(Blocks.cyclone, { shotChance: 0.12 });
+    di(Blocks.spectre, { shotChance: 0.15 });
+    d(Blocks.meltdown, Blocks.meltdown.shootType, { shotChance: 0.03 });
 
     return {
         random() { return all[randomInt(0, all.length)]; }
