9d61592dccdc8f139925af0ea8690e89748d6c50
abomb4 <yangrlx@gmail.com>
2022-09-17 21:54:38 +0800

    update package.sh

--- package.sh
+++ package.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-zip -r mod.zip README.md preview.png icon.png LICENSE mod.hjson bundles/ content/ scripts/ sounds/ sprites/ schematics/
+version=$(cat mod.hjson |grep -e "^version:" |awk '{print $2}')
+zip -r super-cheat-v$version.zip README.md preview.png icon.png LICENSE mod.hjson bundles/ content/ scripts/ sounds/ sprites/ schematics/
